1a85a5288d5032ba4c4e30d66d061d065a5654a5
muhammadwaqas96 <muhammadwaqas96845@gmail.com>
2025-09-24 00:22:37 +0500

    auth file is completed

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,1 +1,56 @@
 import conf from "../conf/conf"
+import { Client, Account, ID } from "appwrite"
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId)
+        this.account = new Account(this.client)
+    }
+
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+
+            if (userAccount) {
+                // call another function
+                return this.login({email, password});
+            } else {
+                return userAccount
+            }
+        } catch(error){
+            return error
+        }
+    }
+
+    async login(){
+        try{
+            return await this.account.createEmailPasswordSession(email, password)
+        }catch(error){
+            return error
+        }
+    }
+
+    async getCurrentUser(){
+        try{
+            return await this.account.getSession()
+        }catch(error){
+            return error
+        }
+    }
+
+    async logout(){
+        try{
+            return await this.account.deleteSession()
+        }catch(error){
+            return error
+        }
+    }
+}
+
+const authService = AuthService()
+export default authService
