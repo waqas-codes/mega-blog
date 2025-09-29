9a36c97b115fda18dd12c697499047910b978556
muhammadwaqas96 <muhammadwaqas96845@gmail.com>
2025-09-30 00:27:49 +0500

    select and postcard components completed

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -9,7 +9,7 @@ function PostCard({$id, title, featuredImage}) {
                 <img src={appwriteService.getFilePreview(featuredImage)} alt="title" 
                 className='rounded-xl'/>
             </div>
-            <h2 className='text-xl font-bold'>{title}</h2>h
+            <h2 className='text-xl font-bold'>{title}</h2>
         </div>
     </link>
   )
