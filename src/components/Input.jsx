89cfb03b8d359e1315d341b81fffa14f31352356
muhammadwaqas96 <muhammadwaqas96845@gmail.com>
2025-10-02 22:22:06 +0500

    input components is completed

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -7,7 +7,7 @@ const input = React.forwardRef(function Input({
     ...props
 }, ref){
     const id = useId()
-    return(
+    return(w
         <div className="w-full">
             {label && <label className="inline-block mb-1 pl-1"
             htmlFor={id}>
