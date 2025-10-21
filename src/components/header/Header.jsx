208f71b8b5993242208b27a60c6af194ad71a77b
muhammadwaqas96 <muhammadwaqas96845@gmail.com>
2025-10-21 21:53:55 +0500

    The issue is in header file

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
   const authStatus = useSelector((state) => state.auth.status)
   const navigate = useNavigate()
 
-  const navItem = [
+  const navItems = [
     {
       name: 'Home',
       slug: '/',
@@ -40,34 +40,30 @@ function Header() {
       <Container>
         <nav className='flex'>
           <div className='mr-4'>
-            <Link to="/">
-            <Logo width='70px'/>
-            </Link>
+            <Link to='/'>
+              <Logo width='70px'   />
+
+              </Link>
           </div>
           <ul className='flex ml-auto'>
-            {
-              navItem.map((item) => 
-               item.active ? (
-                <li key={item.name}>
-                  <button 
-                  onClick={() => navigate(item.slug)}
-                  className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>
-                    {item.name}
-                  </button>
-                </li>
-               ) : null
-              )
-            }
-            {
-              authStatus && (
-                <li>
-                  <LogoutBtn/>
-                </li>
-              )
-            }
+            {navItems.map((item) => 
+            item.active ? (
+              <li key={item.name}>
+                <button
+                onClick={() => navigate(item.slug)}
+                className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                >{item.name}</button>
+              </li>
+            ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
           </ul>
         </nav>
-      </Container>
+        </Container>
     </header>
   )
 }
